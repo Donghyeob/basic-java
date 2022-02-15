e3e8706e2386db5bbb36f54f3c24a5829d998440
donghyeob <donghyeob.k@gmail.com>
2022-02-15 20:35:47 +0900

    comment

--- json/JacksonExample.java
+++ json/JacksonExample.java
@@ -15,33 +15,34 @@ import java.util.Map;
 
 public class JacksonExample {
     public static void main(String[] args) throws IOException {
-        new JacksonExample();
-    }
-
-    public JacksonExample() throws IOException {
+        // resource에 있는 json file
         JsonResource jsonResource = new JsonResource();
         String bossJson = jsonResource.bossJson;
 
+        // ObjectMapper - Jackson library에 포함되어있는 class
         ObjectMapper objectMapper = new ObjectMapper();
         List<Map<String, Object>> map = null;
 
+        // Json String -> Java Object
         map = objectMapper.readValue(bossJson, new TypeReference<List<Map<String, Object>>>(){});
         System.out.println(bossJson.getClass().getName());
 
-        System.out.println("json string -> java object");
+        System.out.println("**********Json String -> Java Object**********");
         System.out.println(map);
         System.out.println(map.getClass().getName()); // java.util.ArrayList;
 
+        // Json Object -> Java Object
         List<Map<String, Object>> map1 = null;
-        map1 = objectMapper.readValue(new File("C:\\Users\\user\\Documents\\java-basic\\json\\resource\\boss.json"), new TypeReference<List<Map<String, Object>>>() {});
+        map1 = objectMapper.readValue(new File("C:\\Users\\dongh\\IdeaProjects\\basic-java\\json\\resource\\boss.json"), new TypeReference<List<Map<String, Object>>>() {});
 
-        System.out.println("json object -> java object");
+        System.out.println("**********Json Object -> Java Object**********");
         System.out.println(map1);
         System.out.println(map1.getClass().getName());
 
+        // Java Object -> Json Object
         String map2 = objectMapper.writeValueAsString(map1);
 
-        System.out.println("java object -> json object");
+        System.out.println("**********Java Object -> Json Object**********");
         System.out.println(map2);
         System.out.println(map2.getClass().getName());
     }
