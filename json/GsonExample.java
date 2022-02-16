cd7cd1074907507971f6e17a6650c5555012e9e7
donghyeob <donghyeob.k@gmail.com>
2022-02-16 19:45:09 +0900

    gson

--- json/GsonExample.java
+++ json/GsonExample.java
@@ -2,9 +2,11 @@ package json;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import json.resource.JsonResource;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,19 +18,37 @@ public class GsonExample {
         // bossJson - Json Array 형태의 String
         JsonResource jsonResource = new JsonResource();
         String bossJson = jsonResource.bossJson;
+        String user = jsonResource.user;
 
         // fromJson - reader로 읽든 string으로 읽든 뒤에 지정된 클래스의 객체로 역직렬화  
         /*Json String -> Json Array*/
         JsonArray jsonArray = gson.fromJson(bossJson, JsonArray.class);
 
-        System.out.println(jsonArray.getClass().getName());
+        System.out.println(jsonArray.getClass().getName()); //com.google.gson.JsonArray
         System.out.println(jsonArray);
 
         /*Json String -> Java ArrayList*/
         List<Map<String, Object>> map = new ArrayList<>();
         map = gson.fromJson(bossJson, ArrayList.class);
 
-        System.out.println(map.getClass().getName());
+        System.out.println(map.getClass().getName()); //java.util.ArrayList
         System.out.println(map);
+
+        /*Json String -> Json Object*/
+        JsonObject jsonObject = gson.fromJson(user, JsonObject.class);
+
+        System.out.println(jsonObject.getClass().getName()); //com.google.gson.JsonObject
+        System.out.println(jsonObject);
+
+        /*Json String -> Java HashMap*/
+        Map<String, Object> map1 = new HashMap<>();
+        map1 = gson.fromJson(user, HashMap.class);
+
+        System.out.println(map1.getClass().getName()); //java.util.HashMap
+        System.out.println(map1);
+
+        /*Json Object or Json Array에서 Java Object로 변환 시 string으로 변환 후
+         * 진행하는 편*/
+
     }
 }
--- json/resource/JsonResource.java
+++ json/resource/JsonResource.java
@@ -636,4 +636,28 @@ public class JsonResource {
             "    \"body\": \"cupiditate quo est a modi nesciunt soluta\\nipsa voluptas error itaque dicta in\\nautem qui minus magnam et distinctio eum\\naccusamus ratione error aut\"\n" +
             "  }\n" +
             "]";
+
+    public String user = "{\n" +
+            "  \"id\": 1,\n" +
+            "  \"name\": \"Leanne Graham\",\n" +
+            "  \"username\": \"Bret\",\n" +
+            "  \"email\": \"Sincere@april.biz\",\n" +
+            "  \"address\": {\n" +
+            "    \"street\": \"Kulas Light\",\n" +
+            "    \"suite\": \"Apt. 556\",\n" +
+            "    \"city\": \"Gwenborough\",\n" +
+            "    \"zipcode\": \"92998-3874\",\n" +
+            "    \"geo\": {\n" +
+            "      \"lat\": \"-37.3159\",\n" +
+            "      \"lng\": \"81.1496\"\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"phone\": \"1-770-736-8031 x56442\",\n" +
+            "  \"website\": \"hildegard.org\",\n" +
+            "  \"company\": {\n" +
+            "    \"name\": \"Romaguera-Crona\",\n" +
+            "    \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
+            "    \"bs\": \"harness real-time e-markets\"\n" +
+            "  }\n" +
+            "}";
 }
