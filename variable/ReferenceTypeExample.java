26196270b6676115b8a23afd68e00129595759e2
donghyeob <donghyeob.k@gmail.com>
2022-02-18 21:55:37 +0900

    referenceType

--- variable/ReferenceTypeExample.java
+++ variable/ReferenceTypeExample.java
@@ -3,10 +3,15 @@ package variable;
 public class ReferenceTypeExample {
     public static void main(String[] args) {
 
-        /*Reference type은 참조 값을 힙 메모리에 저장 */
+        /*Reference type은 참조 값을 힙 메모리에 저장, 크기가 동적인 것들이 저장됨
+        * 직접 값을 담고있지 않고 값을 가리키는 주소를 담고있음
+        * object, class, array, interface, enum, 등
+        * 보통 new 키워드를 이용하여 객체를 생성해 데이터가 생성된 주소를 참조함
+        *   String 제외*/
+
         Object obj = new Object();
-        System.out.println(obj.getClass().getName());
         int[] arr = new int[5];
-        System.out.println(arr.getClass().getName());
+        String str = "AaBbCcDd";
+
     }
 }
