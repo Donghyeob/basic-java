f1b31fce938fdbee831ff719d884a55ea52aac83
donghyeob <donghyeob.k@gmail.com>
2022-03-23 20:25:57 +0900

    extends example

---
+++ extend/extendExam.java
@@ -0,0 +1,33 @@
+package extend;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
+}
+
+class SmartTv extends Tv {
+    boolean caption;
+    void displayCaption(String text) {
+        if(caption) {
+            System.out.println(text);
+        }
+    }
+}
+
+public class extendExam {
+    public static void main(String[] args) {
+        SmartTv stv = new SmartTv();
+
+        stv.channel = 10;
+        stv.channelUp();
+        System.out.println(stv.channel); // 11
+
+        stv.displayCaption("Hello, extend"); // 출력되지 않음
+        stv.caption = true; // 자막이 true 일 때 println(text)
+        stv.displayCaption("Hello, Extend!!!!");
+    }
+}
