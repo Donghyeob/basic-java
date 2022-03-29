13fa32903cb4fc99b0e2b1db0ec7931909f27058
donghyeob <donghyeob.k@gmail.com>
2022-03-30 08:16:21 +0900

    overriding

---
+++ overriding/overridingExample.java
@@ -0,0 +1,24 @@
+package overriding;
+
+public class overridingExample {
+    public static void main(String[] args) {
+        class Point {
+            int x = 1;
+            int y = 2;
+
+            String getLocation() {
+                return "x : " + x + ", y : " + y;
+            }
+        }
+
+        class Point3D extends Point {
+            int z = 3;
+
+            String getLocation() {
+                return "x : " + x + ", y : " + y + ", z : " + z; // 오버라이딩 getLocation() 반환 값에 ", z : " + z를 추가
+            }
+        }
+
+        System.out.println(new Point3D().getLocation());
+    }
+}
